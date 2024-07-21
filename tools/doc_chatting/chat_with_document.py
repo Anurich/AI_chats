8f19af09ee9c72aed08608fa6e4908aad08181c4
Anurich <nautiyalanupam98@gmail.com>
2024-07-21 20:22:15 +0530

    added

--- tools/doc_chatting/chat_with_document.py
+++ tools/doc_chatting/chat_with_document.py
@@ -78,16 +78,16 @@ class Chatwithdocument(CustomLogger):
         retriever_with_rag_fusion = (lambda x: x["question"]) | ragfusion_chain
         rag_chain_with_source = RunnablePassthrough.assign(context=retriever_with_rag_fusion).assign(answer = rag_chain)
         output = rag_chain_with_source.invoke({"question": query})
-        print(output)
-        self.chatHistory.append_data_to_history(query, output)
+        self.chatHistory.append_data_to_history(query, output["answer"])
         #let's take always top last 5 in chat history 
         # to find the answer
         token_sentiment_response = self.sentiment_token_classification(self.llm, output)
         for token in token_sentiment_response[:-2]:
             output = output.replace(token.strip(), f"<<<<{token.strip()}>>>>")
         output += "\n **Sentiment:**\n "+token_sentiment_response[-1]
-
-        return [output,  self.chatHistory.chat_history]
+        
+        metadata = output["context"][0].metadata
+        return [output+f" ***{metadata}***",  self.chatHistory.chat_history]
 
     def sentiment_token_classification(self, llm, content):
         """
