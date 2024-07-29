7df5c89e0fdfe2b6a25225cc55ec9f203ce9c1f6
Anurich <nautiyalanupam98@gmail.com>
2024-07-29 14:23:16 +0530

    added

--- tools/doc_chatting/chat_with_document.py
+++ tools/doc_chatting/chat_with_document.py
@@ -30,7 +30,6 @@ class Chatwithdocument(CustomLogger):
         #self.compressor = LLMLinguaCompressor(model_name="openai-community/gpt2", device_map="cpu")
         self.key = json.load(open("openai_keys/openai_cred.json", "r"))["API_COHERE_KEY"]
         self.nlp = spacy.load("en_core_web_sm")
-
         #self.reranker = RAGPretrainedModel.from_pretrained("colbert-ir/colbertv2.0")
 
     def reciprocal_rank_fusion(self, results: list[list], k=30):
