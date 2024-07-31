5b8fbc15d69932c5a7192457d2c682d45b7efcf7
Anurich <nautiyalanupam98@gmail.com>
2024-07-31 14:09:12 +0530

    added

--- tools/doc_chatting/search_by_descrp_keyword.py
+++ tools/doc_chatting/search_by_descrp_keyword.py
@@ -62,7 +62,7 @@ class Filesearchbykeyworddescrp(CustomLogger):
             metadata = content.metadata
             file_name = metadata["source"]
             page_name = metadata["page"]
-            output = self.chain.invoke({"Context": content.page_content, "description": description})
+            output = self.chain.invoke({"pdf_name": file_name,"Context": content.page_content, "description": description})
             print(output)
             pdf_name, probability = output.split(":")
             if relevance_score.get(pdf_name) == None:
