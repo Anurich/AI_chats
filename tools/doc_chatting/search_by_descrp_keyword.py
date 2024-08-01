3bcb1ad676d412b1d55e251cc855b6ee85529911
Anurich <nautiyalanupam98@gmail.com>
2024-08-01 15:18:58 +0530

    added

--- tools/doc_chatting/search_by_descrp_keyword.py
+++ tools/doc_chatting/search_by_descrp_keyword.py
@@ -124,6 +124,8 @@ class Filesearchbykeyworddescrp(CustomLogger):
         all_outputs =[]
         for rg_doc, score in tqdm(rag_output):           
             output = self.chain.invoke({"pdf_name": rg_doc.metadata["source"],"Context": rg_doc.page_content, "description": description})
+            print(output)
+            print(type(output))
             pdf_name = output["pdf_name"]
             probability = float(output["probability"])
             explaination = output["explaination"]
