1d407056b87b07423362bf09b7df205236973d3e
Anurich <nautiyalanupam98@gmail.com>
2024-08-06 11:42:42 +0530

    added

--- tools/doc_chatting/search_by_descrp_keyword.py
+++ tools/doc_chatting/search_by_descrp_keyword.py
@@ -176,8 +176,8 @@ class Filesearchbykeyworddescrp(CustomLogger):
                     # we need to save into cache 
                     self.llm_cache_in_semantic_memory.add_query_response(description, relevance_score)
             elif cache_response != None:
-                print(cache_response)
-                relevance_score = json.loads(cache_response)
+                corrected_str = re.sub(r"(?<!\\)'", '"', cache_response)
+                relevance_score = json.loads(corrected_str)
 
             html = self.generate_html_table_with_graph(relevance_score)
             end_time = time.time()
