6999e6452c209190b1ac7da445b7cbde524d5aba
Anurich <nautiyalanupam98@gmail.com>
2024-08-12 17:25:40 +0530

    added

--- utils/llm_cache.py
+++ utils/llm_cache.py
@@ -45,6 +45,7 @@ class SemanticMemory:
         )
         
         nearest_distance = results["distances"][0]
+        print(nearest_distance, "**"*10)
         if len(nearest_distance) > 0:
             if nearest_distance[0] < threshold:
                 return results["metadatas"][0][0]["response"]
