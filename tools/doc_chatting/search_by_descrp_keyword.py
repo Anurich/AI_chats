d0afed68f747b362eac11c73497cb58797bd1bb6
Anurich <nautiyalanupam98@gmail.com>
2024-08-13 22:28:55 +0530

    added

--- tools/doc_chatting/search_by_descrp_keyword.py
+++ tools/doc_chatting/search_by_descrp_keyword.py
@@ -34,8 +34,9 @@ class Filesearchbykeyworddescrp(CustomLogger):
 
     def add_file_to_db(self, file_paths):
         assert len(file_paths) >= 1, f"At least have one file!"
-        print(file_paths, "**"*10)
-        for file_path in tqdm(file_paths["filename"]):
+
+        for path in file_paths:
+            file_path = path["filename"]
             if file_path.endswith("pdf"):
                 temp_file_path = self.client.download_file_to_temp(file_path)
                 self.log_info("File Downloaded from bucket to temp folder!")
