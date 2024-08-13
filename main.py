0f7e5ef1ea6fce7fd8e1691eccf7aeb9e5c45e82
Anurich <nautiyalanupam98@gmail.com>
2024-08-13 14:51:27 +0530

    added

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ from utils.bucket import BucketDigitalOcean
 from spacy.cli import download
 import nltk
 nltk.download('punkt_tab')
+nltk.download('averaged_perceptron_tagger_eng')
 from tools.doc_chatting.search_by_descrp_keyword import Filesearchbykeyworddescrp
 download("en_core_web_lg")
 
