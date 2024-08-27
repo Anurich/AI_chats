8a097461630b0eb3db311714d01ebe0e506596ca
Anurich <nautiyalanupam98@gmail.com>
2024-08-27 15:36:40 +0530

    added

--- main.py
+++ main.py
@@ -273,7 +273,7 @@ async def delete_vector_db(requestQuery: QueryRequest):
 
 
     
-@app.post("ai/model/pdf_comparision")
+@app.post("/ai/model/pdf_comparision")
 async def pdf_comparision_two_files(requestQuery: QueryRequest):
     pdfComparision =PdfPreprocessingForComparision(llm=llm, client=client, doc_object=requestQuery.file_names)
 
