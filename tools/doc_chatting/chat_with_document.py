73836d58cf7cc3a65fe9bb9185fd4054f79c30b5
Anurich <nautiyalanupam98@gmail.com>
2024-10-03 15:22:10 +0530

    added

--- tools/doc_chatting/chat_with_document.py
+++ tools/doc_chatting/chat_with_document.py
@@ -94,7 +94,7 @@ class Chatwithdocument(CustomLogger):
                 # Let's take always top last 5 in chat history 
                 # to find the answer
                 
-                output = json.loads(output["answer"])
+                output = json.loads(output["answer"].replace("```json").replace("```", ""))
                 print("*"*199)
                 print(output)
                 ner   = self.nlp(output["answer"])
