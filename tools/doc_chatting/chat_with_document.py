b43ced97d60d782594fa4a42d1d9f527885c7c4e
Anurich <nautiyalanupam98@gmail.com>
2024-10-09 11:05:12 +0530

    added

--- tools/doc_chatting/chat_with_document.py
+++ tools/doc_chatting/chat_with_document.py
@@ -8,10 +8,9 @@ from langchain.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_community.callbacks import get_openai_callback
 from utils.llm_cache import SemanticMemory
-from utils.utility import parse_complex_string
+from utils.utility import parse_response_list
 from langchain_openai.embeddings import OpenAIEmbeddings
 from utils import history, prompts
-import ast
 import time
 from typing import List
 from langchain_community.vectorstores import Chroma
@@ -112,7 +111,7 @@ class Chatwithdocument(CustomLogger):
                 response_list=[output_answer+f" ***{output["source"]}*** ----{tokens_with_label}----",  self.chatHistory.chat_history]
                 self.llm_cache_in_semantic_memory.add_query_response(query, response_list)
             elif cache_response != None:
-                response_list = parse_complex_string(cache_response)
+                response_list = parse_response_list(cache_response)
             end_time = time.time()
             print(f"Total Time Taken: {end_time - start_time:0.2f}")
             print(f"{cb}")
