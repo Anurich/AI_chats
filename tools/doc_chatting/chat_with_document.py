93d5afa14bd4eee02612e5b93d043fbb458581be
Anurich <nautiyalanupam98@gmail.com>
2024-10-09 15:07:32 +0530

    added

--- tools/doc_chatting/chat_with_document.py
+++ tools/doc_chatting/chat_with_document.py
@@ -112,6 +112,7 @@ class Chatwithdocument(CustomLogger):
                 response_list=[output_answer+f" ***{output["source"]}*** ----{tokens_with_label}----",  self.chatHistory.chat_history]
                 output["tokens_with_label"] = tokens_with_label
                 output["chat_history"] = self.chatHistory.chat_history
+                print(output)
                 self.llm_cache_in_semantic_memory.add_query_response(query, output)
             elif cache_response != None:
                 output = json.loads(output)
